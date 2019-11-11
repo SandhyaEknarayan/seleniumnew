6b79c130d917e6f8115a8198f26cbd086c4e1878
Ananta <Ananta@DESKTOP-KPPT1LI>
2019-11-11 22:21:27 +0530

    commit my new changes

--- src/selenium/newpac/Fblogin.java
+++ src/selenium/newpac/Fblogin.java
@@ -13,6 +13,7 @@ public class Fblogin {
  
 		WebDriver driver= new FirefoxDriver();
 		 driver.get("https://www.facebook.com");
+		 System.out.println("fb page opened");
 		 driver.manage().window().maximize();
 		 Thread.sleep(1000);
 		 System.out.println("page opened successfully");
