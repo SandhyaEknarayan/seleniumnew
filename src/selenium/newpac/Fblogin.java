726bfd052809bb279406fe1a1e7d90cdd30daf5a
Ananta <Ananta@DESKTOP-KPPT1LI>
2019-11-11 23:32:37 +0530

    sw

--- src/selenium/newpac/Fblogin.java
+++ src/selenium/newpac/Fblogin.java
@@ -10,7 +10,7 @@ public class Fblogin {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		 System.setProperty("webdriver.gecko.driver", "E:\\Sandhya_Java\\Software\\geckodriver.exe");
-		 System.out.println("This is  not an comment at 11:20 PM...");
+		 System.out.println("This is not an comment at 11:20 PM...");
 		WebDriver driver= new FirefoxDriver();
 		 driver.get("https://www.facebook.com");
 		
